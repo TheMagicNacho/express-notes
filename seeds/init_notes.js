04ffc70df63318641c68f408c5f333d51585a33c
Justin Peter <justinhasanaccount@gmail.com>
2022-03-09 11:28:18 -0500

    dry seed

--- seeds/init_notes.js
+++ seeds/init_notes.js
@@ -2,10 +2,12 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+const table = 'notes_table';
+
 export async function seed(knex) {
   // Deletes ALL existing entries
-  await knex('notes_table').del()
-  await knex('notes_table').insert([
+  await knex(table).del()
+  await knex(table).insert([
     { title: 'test1', data: "Cool stuff goes here -- wow" },
     { title: 'test2', data: "Cool stuff goes here -- wow" },
     { title: 'test3', data: "Cool stuff goes here -- wow" }
